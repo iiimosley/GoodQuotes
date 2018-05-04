e95730fb5efcbc46e2fd9b41eac2ccd7ef0a7dd7
Trey Mosley <iiimosley@gmail.com>
2018-05-04 15:13:45 -0500

    refactor for title

--- server/controllers/titleCtrl.js
+++ server/controllers/titleCtrl.js
@@ -16,29 +16,27 @@ module.exports.getTitleQuotes = (req, res, next) => {
 
     let quoteData = {};
     let pageNumArr = $('.next_page')[0].parent.children
-    quoteData.total_pages = pageNumArr[pageNumArr.length - 3].children[0].data;
-    let allQuotes = [];
+    quoteData.current_page = !req.query.page ? 1 : +req.query.page;
+    quoteData.total_pages = +pageNumArr[pageNumArr.length - 3].children[0].data;
+    quoteData.quotes = [];
 
     $('.quoteText').each((i, el) => {
-
       let newQuote = {};
-
-      if ($(el).find('.authorOrTitle').length<2){
+      if ($(el).find('.authorOrTitle').length < 2) {
         newQuote = {
-          quote: el.children[0].data.trim().replace(/[“”"\"]/g, ''),
+          quote: $(el).text().split('—')[0].trim().replace(/[“”"\"]/g, ''),
           author: $(el).find('.authorOrTitle')[0].children[0].data,
           publication: null,
         };
       } else {
         newQuote = {
-          quote: el.children[0].data.trim().replace(/[“”"\"]/g, ''),
+          quote: $(el).text().split('—')[0].trim().replace(/[“”"\"]/g, ''),
           author: $(el).find('.authorOrTitle')[0].children[0].data,
           publication: $(el).find('.authorOrTitle')[1].children[0].data,
         };
       }
-      allQuotes.push(newQuote);
+      quoteData.quotes.push(newQuote);
     });
-    quoteData.quotes = allQuotes;
     res.status(200).json(quoteData);
   })
   .catch(err => res.status(500).json(err));
