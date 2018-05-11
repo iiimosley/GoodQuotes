783ff0ae5788f190f466c4074912282771a8cd65
Trey Mosley <iiimosley@gmail.com>
2018-05-10 20:07:20 -0500

    page rendering; separate servers

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 
-const routes = require('./routes');
-const port = process.env.PORT || 8081;
+const routes = require('./api/routes');
+const port = process.env.PORT || 8080;
 
 // static rendering
 app.use(cors());
 app.use(express.static(__dirname + "/public"));
-app.use("/vue", express.static(__dirname + "server/node_modules/vue/"));
+app.use("/vue", express.static(__dirname + "node_modules/vue/"));
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/components/Home';
-import SearchQuotes from '@/components/SearchQuotes';
-import SmartQuote from '@/components/SmartQuote';
-import Developer from '@/components/Developer';
+import Home from '../components/Home';
+import SearchQuotes from '../components/SearchQuotes';
+import SmartQuote from '../components/SmartQuote';
+import Developer from '../components/Developer';
 
 Vue.use(Router);
 
